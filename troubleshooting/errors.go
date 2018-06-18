db72715bc2785bb9e0ff8d7125b322b17f7ac89a
K.Lin <l714.100@gmail.com>
2018-06-18 17:30:59 +0800

    3-15: Update Errors Notification

--- troubleshooting/errors.go
+++ troubleshooting/errors.go
@@ -7,17 +7,21 @@ import (
 	"time"
 )
 
-var errCrewNotFound = errors.New("Crew member not found")
+// ErrCrewNotFound <-Share between packages
+var ErrCrewNotFound = errors.New("Crew member not found")
 
 var scMapping = map[string]int{
-	"RUS-KSA": 5,
-	"EGY-URU": 1,
-	"MRC-IRN": 1,
-	"POR-ESP": 6,
-	"FRA-AUS": 3,
-	"ARG-ISL": 2,
-	"PER-DNK": 1,
-	"CRO-NGA": 2,
+	"RUS-KSA":  5,
+	"EGY-URU":  1,
+	"*MRC-IRN": 1,
+	"POR-ESP":  6,
+	"FRA-AUS":  3,
+	"ARG-ISL":  2,
+	"PER-DNK":  1,
+	"CRO-NGA":  2,
+	"CRC-SRB":  1,
+	"GER-MEX":  1,
+	"*BRA-SUI": 2,
 }
 
 type findError struct {
@@ -31,7 +35,7 @@ func (e findError) Error() string {
 func findSC(game, server string) (int, error) {
 	time.Sleep(time.Duration(rand.Intn(15)) * time.Second) // Simulate seraching
 	if v, ok := scMapping[game]; !ok {
-		return -1, errors.New("Crew member not found")
+		return -1, fmt.Errorf("%s could not be found on server '%s'", game, server)
 	} else {
 		return v, nil
 	}
@@ -39,7 +43,9 @@ func findSC(game, server string) (int, error) {
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	clearance, err := findSC("CRC-SRB", "server 1")
-	fmt.Println("Clearance level found: ", clearance)
-	fmt.Println("Error code: ", err)
+	if clearance, err := findSC("BRA-SUI", "server 1"); err != nil {
+		fmt.Println("Error Occured: [Type]", ErrCrewNotFound, "[Msg]", err)
+	} else {
+		fmt.Println("Clearance level found: ", clearance)
+	}
 }
