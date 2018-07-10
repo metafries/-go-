126560df672c9cd55b0d68596aa78ba7d26286ea
K.Lin <l714.100@gmail.com>
2018-07-10 23:14:05 +0800

    Timers and Tickers: Tick Counter_3

---
+++ TImers&Tickers/TickCounter3.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(1 * time.Second)
+	done := make(chan bool)
+	go TickCounter(ticker, done)
+	time.Sleep(5 * time.Second)
+	ticker.Stop()
+	done <- true
+	time.Sleep(10 * time.Second)
+	fmt.Println("Exiting ...")
+}
+
+func TickCounter(ticker *time.Ticker, done chan bool) {
+	i := 0
+Loop:
+	for {
+		select {
+		case t := <-ticker.C:
+			i++
+			fmt.Println("Count ", i, " at ", t)
+		case <-done:
+			fmt.Println("Done signal")
+			break Loop
+		}
+	}
+	fmt.Println("Exiting the tick counter")
+}
