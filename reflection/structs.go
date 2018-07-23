6dbd3e313c99095104d9ed95a2a00096c1a17cbd
K.Lin <l714.100@gmail.com>
2018-07-23 21:41:11 +0800

    Laws of Reflection_6: Tags of Struct
    [20-22] Error Handling
    [*] Renamed Folders:
    Syncs&Locks -> SyncsLocks
    TImers&Tickers -> TimersTickers

--- reflection/structs.go
+++ reflection/structs.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	type myStruct struct {
-		Field1 int
-		Field2 string
-		Field3 float64
+		Field1 int     `alias:"f1" desc:"TIME"`
+		Field2 string  `alias:"f2" desc:"CLUBvCLUB"`
+		Field3 float64 `alias:"f3" desc:"DATE"`
 	}
 	mys := myStruct{1005, "ROMAvTOT", 7.26}
 	InspectStructType(mys)
@@ -17,10 +17,15 @@ func main() {
 
 func InspectStructType(i interface{}) {
 	mysRValue := reflect.ValueOf(i)
+	if mysRValue.Kind() != reflect.Struct {
+		return
+	}
 	mysRType := reflect.TypeOf(i)
 	for i := 0; i < mysRType.NumField(); i++ {
 		fieldRType := mysRType.Field(i)
 		fieldRValue := mysRValue.Field(i)
-		fmt.Printf("Name: %s | Type: %s | Value: %v \n", fieldRType.Name, fieldRType.Type, fieldRValue.Interface())
+		fmt.Printf("Name: %s | Type: %s | Value: %v | Tag(alias): %s | Tag(desc): %s \n",
+			fieldRType.Name, fieldRType.Type, fieldRValue.Interface(), fieldRType.Tag.Get("alias"), fieldRType.Tag.Get("desc"))
+
 	}
 }
