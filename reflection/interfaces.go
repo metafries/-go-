e8b9d531625fe207b1d27ad33e7a9b56ec412c97
K.Lin <l714.100@gmail.com>
2018-07-23 23:51:59 +0800

    Laws of Reflection_8: Check if a struct implements a given interface

---
+++ reflection/interfaces.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Printer interface {
+	Print(s string)
+}
+
+type pStruct struct {
+	s string
+}
+
+func (p *pStruct) Print(s string) {
+	p.s = s
+	fmt.Println(s)
+}
+
+func main() {
+	p := new(pStruct)
+	inspectType(p)
+}
+
+func inspectType(obj interface{}) {
+	v := reflect.ValueOf(obj)
+	t := v.Type()
+	myInterface := reflect.TypeOf((*Printer)(nil)).Elem()
+	fmt.Println("==> obj implements Printer?", t.Implements(myInterface))
+}
