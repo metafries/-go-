1a81c78c818ff62166980a04f4c712dbdc2f7c34
K.Lin <l714.100@gmail.com>
2018-08-04 23:26:42 +0800

    Format_4: JSON: Convert Map

--- format/JSON.go
+++ format/JSON.go
@@ -23,23 +23,26 @@ func main() {
 	cm4 := CrewMember{1, "MetaFries", 10, []string{"GIR", "TOT"}}
 	cm3 := CrewMember{Name: "Cashviar", SecurityClearance: 20, AccessCodes: []string{"NEW", "TOT"}}
 	si3 := ShipInfo{1, "Fighter", cm3}
+	msk2 := map[string]int{"item1": 1, "item2": 2}
+	mik2 := map[int]string{1: "item1", 2: "item2"} // non-string map keys (Go 1.7 -)
 
 	b4, err := json.Marshal(&cm4)
-	PrintError(err)
+	PrintResult(err, b4)
 	b3, err := json.Marshal(&cm3)
-	PrintError(err)
+	PrintResult(err, b3)
 	bsi3, err := json.Marshal(&si3)
-	PrintError(err)
-
-	fmt.Println(string(b4))
-	fmt.Println(string(b3))
-	fmt.Println(string(bsi3))
+	PrintResult(err, bsi3)
+	bmsk2, err := json.Marshal(&msk2)
+	PrintResult(err, bmsk2)
+	bmik2, err := json.Marshal(&mik2)
+	PrintResult(err, bmik2)
 
 }
 
-func PrintError(err error) {
+func PrintResult(err error, b []byte) {
 	if err != nil {
 		fmt.Println("[ERROR]", err)
 		return
 	}
+	fmt.Println(string(b))
 }
