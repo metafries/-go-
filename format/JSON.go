e84cf4ea56cd3f7f33d635f07aac5390b41a464c
K.Lin <l714.100@gmail.com>
2018-08-04 21:57:18 +0800

    Format_3: JSON: Nested Struct

--- format/JSON.go
+++ format/JSON.go
@@ -14,16 +14,26 @@ func main() {
 		AccessCodes       []string `json:"accesscodes"`
 	}
 
+	type ShipInfo struct {
+		ShipID    int
+		ShipClass string
+		Captain   CrewMember
+	}
+
 	cm4 := CrewMember{1, "MetaFries", 10, []string{"GIR", "TOT"}}
 	cm3 := CrewMember{Name: "Cashviar", SecurityClearance: 20, AccessCodes: []string{"NEW", "TOT"}}
+	si3 := ShipInfo{1, "Fighter", cm3}
 
 	b4, err := json.Marshal(&cm4)
 	PrintError(err)
 	b3, err := json.Marshal(&cm3)
 	PrintError(err)
+	bsi3, err := json.Marshal(&si3)
+	PrintError(err)
 
 	fmt.Println(string(b4))
 	fmt.Println(string(b3))
+	fmt.Println(string(bsi3))
 
 }
 
