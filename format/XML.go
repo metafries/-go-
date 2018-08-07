dbb51a6c52f2891c6792ecc37012282a350edb86
K.Lin <l714.100@gmail.com>
2018-08-07 18:42:18 +0800

    Format_10: XML: Custom Struct Tags

--- format/XML.go
+++ format/XML.go
@@ -8,20 +8,21 @@ import (
 func main() {
 
 	type CrewMember struct {
-		ID                int      `xml:"id,omitempty"`
-		Name              string   `xml:"name"`
+		ID                int      `xml:"id"`
+		Name              string   `xml:"name,attr"`
 		SecurityClearance int      `xml:"clearancelevel"`
 		AccessCodes       []string `xml:"accesscodes"`
 	}
 
 	type ShipInfo struct {
+		XMLName   xml.Name `xml:"SHIPINFO"`
 		ShipID    int
 		ShipClass string
 		Captain   CrewMember
 	}
 
 	cm3 := CrewMember{Name: "Cashviar", SecurityClearance: 20, AccessCodes: []string{"NEW", "TOT"}}
-	si3 := ShipInfo{1, "Fighter", cm3}
+	si3 := ShipInfo{ShipID: 1, ShipClass: "Fighter", Captain: cm3}
 
 	bsi3, err := xml.MarshalIndent(&si3, "", "    ")
 	PrintResult(err, bsi3)
