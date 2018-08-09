3b4567ea7bf6dec406b347771c31dc39c80f2607
K.Lin <l714.100@gmail.com>
2018-08-09 22:33:12 +0800

    Format_17: CSV: [func] strconv.Atoi

--- MF/encoders/csv/reader.go
+++ MF/encoders/csv/reader.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -17,22 +19,28 @@ func main() {
 
 	r := csv.NewReader(file)
 	r.Comment = '#'
-	r.Comma = ';'
+	// r.Comma = ';'
 
-	records, err := r.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(records)
-
-	// for {
-	// 	record, err := r.Read()
-	// 	if err == io.EOF {
-	// 		break
-	// 	}
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Println("    CSV Row", record)
+	// records, err := r.ReadAll()
+	// if err != nil {
+	// 	log.Fatal(err)
 	// }
+	// fmt.Println(records)
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("    CSV Row", record)
+
+		i, err := strconv.Atoi(record[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(i * 4)
+	}
 }
