20b9bf752b0447095799b70556dde701d2515fa8
K.Lin <l714.100@gmail.com>
2018-08-20 17:04:07 +0800

    Database_5: MongoDB: [mgo] Collection.Find

--- MF/dbhandler/mongodb/mongodb.go
+++ MF/dbhandler/mongodb/mongodb.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type clubInfo struct {
@@ -30,4 +31,8 @@ func main() {
 	n, _ := clubinfo.Count()
 	log.Println("[INFO] mgo - [Collection.Count]: Number of docs of club_info is", n)
 
+	// Perform simple query.
+	ci := clubInfo{}
+	clubinfo.Find(bson.M{"id": 1}).One(&ci)
+	log.Println("[INFO] mgo - [Collection.Find]: Docs of club_info at id = 1 is", ci)
 }
