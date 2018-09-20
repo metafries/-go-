1097fa1500f8e9ecfbc7ac7d38025416c5b2ccf0
K.Lin <l714.100@gmail.com>
2018-09-20 23:54:21 +0800

    WebApp_5: RequestBin: [http] NewRequest: PUT

--- MF/mfweb/clienttesters/webclient.go
+++ MF/mfweb/clienttesters/webclient.go
@@ -1,26 +1,39 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	url := "http://localhost:8000/1ei30n11"
-	resp, err := http.Get(url)
-	inspectResponse(resp, err)
+	// resp, err := http.Get(url)
+	// inspectResponse(resp, err)
 
-	data, err := json.Marshal(struct {
-		X int
-		Y float32
-	}{X: 4, Y: 3.8})
-	if err != nil {
-		log.Fatal("FATAL: Error Occured While Marshaling JSON ", err)
+	// data, err := json.Marshal(struct {
+	// 	X int
+	// 	Y float32
+	// }{X: 4, Y: 3.8})
+	// if err != nil {
+	// 	log.Fatal("FATAL: Error Occured While Marshaling JSON ", err)
+	// }
+	// resp, err = http.Post(url, "application/json", bytes.NewReader(data))
+	// inspectResponse(resp, err)
+
+	client := http.Client{
+		Timeout: 3 * time.Second,
 	}
-	resp, err = http.Post(url, "application/json", bytes.NewReader(data))
+	// client.Get(url)
+
+	req, err := http.NewRequest(http.MethodPut, url, nil)
+	if err != nil {
+		log.Fatal("FATAL: ", err)
+	}
+	req.Header.Add("x-testheader", "Testing Add Header")
+	req.Header.Set("User-Agent", "Testing Set Header")
+	resp, err := client.Do(req)
 	inspectResponse(resp, err)
 }
 
